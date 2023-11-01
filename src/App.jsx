579eb2629e22313e9ce7476cf8717b2a8b4eaa6d
Alapottra <alapottratrojon@gmail.comgit config --global user.email alapottratrojon@gmail.comgit config --gloval user.name alapottra-troj1n>
2023-11-01 13:09:31 +0600

    added upload mockup feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useRef, useState } from "react";
 import Product from "./Product.jsx";
-
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 const App = () => {
+  const [animationParent] = useAutoAnimate();
   const [products, setProducts] = useState([
     {
       id: "Product-11",
@@ -59,29 +60,43 @@ const App = () => {
       path: "/images/image-1.webp",
       order: 11,
     },
+    
   ]);
-  const [dragId, setDragId] = useState();
   const [selectedProducts, setSelectedProducts] = useState([]);
+  const [dragId, setDragId] = useState();
+
+  console.log(products)
+
+  // Filter out the products that have an id that is not in the selectedProducts state
+  const handleRemoveSelected = () => {
+    const updatedProducts = products.filter(
+      (product) => !selectedProducts.includes(product.id)
+    );
+
+    setProducts(updatedProducts);
+
+    setSelectedProducts([]);
+  };
 
   //functions to handle drag and drop
   const handleDrag = (e) => {
     setDragId(e.currentTarget.id);
   };
   const handleDrop = (e) => {
-    const dragBox = products.find((product) => product.id === dragId);
-    const dropBox = products.find(
+    const dragProduct = products.find((product) => product.id === dragId);
+    const dropProduct = products.find(
       (product) => product.id === e.currentTarget.id
     );
 
-    const dragBoxOrder = dragBox.order;
-    const dropBoxOrder = dropBox.order;
+    const dragProductOrder = dragProduct.order;
+    const dropProductOrder = dropProduct.order;
 
     const newProductsState = products.map((product) => {
       if (product.id === dragId) {
-        product.order = dropBoxOrder;
+        product.order = dropProductOrder;
       }
       if (product.id === e.currentTarget.id) {
-        product.order = dragBoxOrder;
+        product.order = dragProductOrder;
       }
       return product;
     });
@@ -89,18 +104,46 @@ const App = () => {
     setProducts(newProductsState);
   };
 
+  const handleFile = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      const newProduct = {
+        id: `Product-${products.length + 1}`,
+        path: URL.createObjectURL(file),
+        order: products.length + 1,
+      };
+
+      setProducts([...products, newProduct]);
+    }
+  };
+
   return (
-    <div className="h-screen bg-slate-200 flex justify-center items-center">
-      <div className=" max-w-6xl mx-auto p-8 lg:w-[60%] ">
-        <div className="bg-slate-50 rounded-lg">
-          <div className=" p-5 border-b border-gray-400">
+    <div className="min-h-screen bg-slate-200 flex justify-center items-center">
+      <div className=" max-w-6xl mx-auto p-8 lg:w-[60%]   ">
+        <div className="bg-slate-50 rounded-lg ">
+          <div className=" p-5 border-b border-gray-400 flex justify-between items-center">
             <h3 className="font-bold">
               {!selectedProducts.length
                 ? "Gallery"
                 : `${selectedProducts.length + " Filed Selected"}`}
             </h3>
+
+            {selectedProducts.length ? (
+              <button
+                onClick={handleRemoveSelected}
+                className="text-red-400 text-sm font-bold"
+              >
+                Delete File
+              </button>
+            ) : (
+              ""
+            )}
           </div>
-          <div className="grid grid-cols-5 gap-5 p-7">
+          <div
+            ref={animationParent}
+            className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 p-7 "
+          >
             {products
               .sort((a, b) => a.order - b.order)
               .map((product) => (
@@ -115,13 +158,14 @@ const App = () => {
                   setSelectedProducts={setSelectedProducts}
                 />
               ))}
-            <div className="border-2 border-gray-300 rounded-lg bg-gray-300 flex flex-col justify-center items-center gap-5 cursor-pointer">
+            <div className="border-2 border-dashed border-gray-300 rounded-lg bg-gray-100 flex flex-col justify-center items-center gap-5 cursor-pointer relative">
               <img
-                 className="w-6"
+                className="w-6"
                 src="https://www.svgrepo.com/show/457965/img-box.svg"
                 alt="img"
               />
-               <p className="text-sm font-bold">Add Images</p>
+              <p className="text-sm font-bold">Add Images</p>
+              <input type="file" className="opacity-0 absolute w-full h-full" name="file-handle" onChange={handleFile} />
             </div>
           </div>
         </div>
--- src/Product.jsx
+++ src/Product.jsx
@@ -7,12 +7,9 @@ const Product = ({
   selectedProducts,
   setSelectedProducts,
 }) => {
-
-
   const isProductSelected = selectedProducts.includes(productId); //to check if product is already selected
 
   const handleToggle = () => {
-
     if (selectedProducts.includes(productId)) {
       setSelectedProducts(
         selectedProducts.filter((product) => productId !== product)
@@ -24,12 +21,11 @@ const Product = ({
 
   return (
     <div
-      onClick={handleToggle}
       className={`${
         order === 1 && "col-span-2 row-span-2  " //different size for featured image
       } cursor-pointer  rounded-xl border-2 border-gray-300 overflow-hidden hover:filter  transition-all
-      ${isProductSelected ? "filter brightness-50" : "hover:brightness-75"}
-      relative group`}
+      ${isProductSelected ? "filter brightness-50" : "hover:brightness-75 "}
+      relative group `}
       draggable={true}
       id={productId}
       onDragOver={(ev) => ev.preventDefault()}
@@ -39,10 +35,11 @@ const Product = ({
       <input
         type="checkbox"
         name="checkebox"
-        className={`absolute top-2 left-2 w-6 h-6 opacity-0 ${
+        className={`absolute top-2 left-2 w-6 h-6 opacity-0 cursor-pointer ${
           isProductSelected ? "opacity-100" : " group-hover:opacity-100"
         }`}
         checked={isProductSelected}
+        onChange={handleToggle}
       />
       <img className="w-full" src={path} alt="product" />
     </div>
