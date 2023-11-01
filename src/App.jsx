9da96dc0e84a6040448583c37e0662d9a6d4a734
Alapottra <alapottratrojon@gmail.comgit config --global user.email alapottratrojon@gmail.comgit config --gloval user.name alapottra-troj1n>
2023-11-01 12:02:55 +0600

    react reorder

---
+++ src/App.jsx
@@ -0,0 +1,121 @@
+import { useRef, useState } from "react";
+import Product from "./Product.jsx";
+
+const App = () => {
+  const [products, setProducts] = useState([
+    {
+      id: "Product-11",
+      path: "/images/image-11.jpeg",
+      order: 1,
+    },
+    {
+      id: "Product-2",
+      path: "/images/image-2.webp",
+      order: 2,
+    },
+    {
+      id: "Product-3",
+      path: "/images/image-3.webp",
+      order: 3,
+    },
+    {
+      id: "Product-4",
+      path: "/images/image-4.webp",
+      order: 4,
+    },
+    {
+      id: "Product-5",
+      path: "/images/image-5.webp",
+      order: 5,
+    },
+    {
+      id: "Product-6",
+      path: "/images/image-6.webp",
+      order: 6,
+    },
+    {
+      id: "Product-7",
+      path: "/images/image-7.webp",
+      order: 7,
+    },
+    {
+      id: "Product-8",
+      path: "/images/image-8.webp",
+      order: 8,
+    },
+    {
+      id: "Product-9",
+      path: "/images/image-9.webp",
+      order: 9,
+    },
+    {
+      id: "Product-10",
+      path: "/images/image-10.jpeg",
+      order: 10,
+    },
+
+    {
+      id: "Product-1",
+      path: "/images/image-1.webp",
+      order: 11,
+    },
+  ]);
+  const [dragId, setDragId] = useState();
+  const [selectedProducts,setSelectedProducts] = useState([]);
+
+  //functions to handle drag and drop
+  const handleDrag = (e) => {
+    setDragId(e.currentTarget.id);
+  };
+  const handleDrop = (e) => {
+    const dragBox = products.find((product) => product.id === dragId);
+    const dropBox = products.find(
+      (product) => product.id === e.currentTarget.id
+    );
+
+    const dragBoxOrder = dragBox.order;
+    const dropBoxOrder = dropBox.order;
+
+    const newProductsState = products.map((product) => {
+      if (product.id === dragId) {
+        product.order = dropBoxOrder;
+      }
+      if (product.id === e.currentTarget.id) {
+        product.order = dragBoxOrder;
+      }
+      return product;
+    });
+
+    setProducts(newProductsState);
+  };
+
+  return (
+    <div className="h-screen bg-slate-200 flex justify-center items-center">
+      <div className=" max-w-6xl mx-auto p-8 lg:w-[60%] ">
+        <div className="bg-slate-50 rounded-lg">
+          <div className=" p-5 border-b border-gray-400">
+            <h3 className="font-bold">Gallery</h3>
+          </div>
+          <div className="grid grid-cols-5 gap-5 p-7">
+            {products
+              .sort((a, b) => a.order - b.order)
+              .map((product) => (
+                <Product
+                  key={product.id}
+                  order={product.order}
+                  productId={product.id}
+                  path={product.path}
+                  handleDrop={handleDrop}
+                  handleDrag={handleDrag}
+                  selectedProducts={selectedProducts}
+                  setSelectedProducts={setSelectedProducts}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Product.jsx
@@ -0,0 +1,49 @@
+const Product = ({
+  path,
+  productId,
+  order,
+  handleDrop,
+  handleDrag,
+  selectedProducts,
+  setSelectedProducts,
+}) => {
+  const isProductSelected = selectedProducts.includes(productId);
+
+  const handleToggle = () => {
+    if (selectedProducts.includes(productId)) {
+      setSelectedProducts(
+        selectedProducts.filter((product) => productId !== product)
+      );
+    } else {
+      setSelectedProducts([...selectedProducts, productId]);
+    }
+  };
+
+  return (
+    <div
+      onClick={handleToggle}
+      className={`${
+        order === 1 && "col-span-2 row-span-2  "
+      } cursor-pointer  rounded-xl border-2 border-gray-300 overflow-hidden hover:filter  transition-all
+      ${isProductSelected ? "filter brightness-50" : "hover:brightness-75"}
+      relative group`}
+      draggable={true}
+      id={productId}
+      onDragOver={(ev) => ev.preventDefault()}
+      onDragStart={handleDrag}
+      onDrop={handleDrop}
+    >
+      <input
+        type="checkbox"
+        name="checkebox"
+        className={`absolute top-2 left-2 w-6 h-6 opacity-0 ${
+          isProductSelected ? "opacity-100" : " group-hover:opacity-100"
+        }`}
+        checked={isProductSelected}
+      />
+      <img className="w-full" src={path} alt="product" />
+    </div>
+  );
+};
+
+export default Product;
